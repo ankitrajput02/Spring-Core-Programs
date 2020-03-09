36481d3d7f4c5328402f5b6e5d8e546476d167c0
Ankit <ankitbrajput02@gmail.com>
2020-03-09 12:26:38 +0530

    rename project names

---
+++ springloosecoupling/src/test/java/com/bridgelabz/loosecoupling/SpringloosecouplingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.loosecoupling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringloosecouplingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
